95460e6639b7ad57e9fb02bfa7e820c6b25259d4
Dvir Asaf <dvir.asaf@live.biu.ac.il>
2021-11-29 20:14:56 +0200

    k

--- multiAgents.py
+++ multiAgents.py
@@ -1,10 +1,11 @@
+# Dvir Asaf 313531113
 # multiAgents.py
 # --------------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -74,7 +75,27 @@ class ReflexAgent(Agent):
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
         "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        # Get a list of all the positions where the food is located on board
+        food_pos = successorGameState.getFood().asList()
+
+        # Init the minimum distance between the Pacman's position and the food to "infinity"
+        distance_to_nearest_food = float("inf")
+
+        # Going over each food on the board and search for the the one with the shortest distance to the pacman
+        for food in food_pos:
+            distance_to_food = manhattanDistance(newPos, food)
+            distance_to_nearest_food = min(distance_to_nearest_food, distance_to_food)
+
+        # Check if there are any ghosts around the pacman
+        for ghost in successorGameState.getGhostPositions():
+            # The ghost is too close, we should return minimum score, therefore we'll return -infinity
+            dist_to_ghost = manhattanDistance(newPos, ghost)
+            if (2 > dist_to_ghost):
+                return -float('inf')
+
+        # The idea behind the score calculation: The smaller the distance between the pacman and the food, the higher
+        # the score, since dividing 1 by that number.
+        return successorGameState.getScore() + 1.0 / distance_to_nearest_food
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -135,7 +156,61 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # agent index set to 0 - pacman
+        # depth is zero at beginning
+        move = self.MaxValue(gameState, 0, 0)
+        # return minmax action
+        return move[0]
+
+    def MaxValue(self, gameState, agentIndex, depth):
+        '''
+        The pacman needs to get the highest possible score
+        '''
+        best_pacman_move = ("max", -float("inf"))
+        for action in gameState.getLegalActions(agentIndex):
+            # The possible actions according to the agent's index
+            successor_move, successor_score = self.get_successor_move(action, agentIndex, depth, gameState)
+
+            # update the highest possible score
+            if successor_score > best_pacman_move[1]:
+                best_pacman_move = successor_move
+        return best_pacman_move
+
+    def get_successor_move(self, action, agentIndex, depth, gameState):
+        game_state_successor = gameState.generateSuccessor(agentIndex, action)
+        depth_successor = depth + 1
+        agent_index_successor = depth_successor % gameState.getNumAgents()
+        successor_score = self.MinMaxAlg(game_state_successor, agent_index_successor, depth_successor)
+        successor_move = (action, successor_score)
+        return successor_move, successor_score
+
+    def MinMaxAlg(self, gameState, agentIndex, depth):
+        # If in the current depth is final we should evaluate the state in which it was found.
+        if gameState.isLose() or \
+                self.depth * gameState.getNumAgents() == depth or \
+                gameState.isWin():
+            return self.evaluationFunction(gameState)
+
+        # Pacman: will try to get the maximum score for himself
+        if agentIndex == 0:
+            return self.MaxValue(gameState, agentIndex, depth)[1]
+
+        # Ghosts: they will try to get the minimum score for the pacman.
+        else:
+            return self.MinValue(gameState, agentIndex, depth)[1]
+
+    def MinValue(self, gameState, agentIndex, depth):
+        """
+        Ghosts want to make the pacman get the lowest score
+        """
+
+        best_ghosts_move = ("min",float("inf"))
+        for action in gameState.getLegalActions(agentIndex):
+            successor_move, successor_score = self.get_successor_move(action, agentIndex, depth, gameState)
+            # update the lowest possible score
+            if successor_score < best_ghosts_move[1]:
+                best_ghosts_move = successor_move
+        return best_ghosts_move
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
@@ -147,6 +222,64 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        inf = float("inf")
+        # agent index set to 0 - pacman
+        # depth is zero at beginning
+        move = self.MaxValue(gameState, 0, 0, -inf, inf)
+        # return minmax action
+        return move[0]
 
+    def get_successor_move(self, action, agentIndex, alpha, beta, depth, gameState):
+        game_state_successor = gameState.generateSuccessor(agentIndex, action)
+        depth_successor = depth + 1
+        agent_index_successor = depth_successor % gameState.getNumAgents()
+        successor_score = self.AlphaBetaAlg(game_state_successor, agent_index_successor, depth_successor, alpha, beta)
+        successor_move = (action, successor_score)
+        return successor_move, successor_score
 
+    def AlphaBetaAlg(self, gameState, agentIndex, depth, alpha, beta):
+        # If the current depth is final we should evaluate the state in which it was found.
+        if gameState.isLose() or \
+                self.depth * gameState.getNumAgents() == depth or \
+                gameState.isWin():
+            return self.evaluationFunction(gameState)
+        # Pacman: will try to maximize alpha
+        if agentIndex == 0:
+            return self.MaxValue(gameState, agentIndex, depth, alpha, beta)[1]
+        # Ghosts: they will try to minimize (beta)
+        else:
+            return self.MinValue(gameState, agentIndex, depth, alpha, beta)[1]
+
+    def MaxValue(self, gameState, agentIndex, depth, alpha, beta):
+        best_pacman_move = ("max", -float("inf"))
+        for action in gameState.getLegalActions(agentIndex):
+            successor_move, successor_score = self.get_successor_move(action, agentIndex, alpha, beta, depth, gameState)
+
+            # update the highest possible score
+            if successor_score > best_pacman_move[1]:
+                best_pacman_move = successor_move
+
+            # Max pruning
+            if beta < best_pacman_move[1]:
+                return best_pacman_move
+            else:
+                alpha = max(best_pacman_move[1], alpha)
+
+        return best_pacman_move
+
+    def MinValue(self, gameState, agentIndex, depth, alpha, beta):
+        best_ghosts_move = ("min", float("inf"))
+        for action in gameState.getLegalActions(agentIndex):
+            successor_move, successor_score = self.get_successor_move(action, agentIndex, alpha, beta, depth, gameState)
+
+            # update the lowest possible score
+            if successor_score < best_ghosts_move[1]:
+                best_ghosts_move = successor_move
+
+            # Min pruning
+            if alpha > best_ghosts_move[1]:
+                return best_ghosts_move
+            else:
+                beta = min(best_ghosts_move[1], beta)
+
+        return best_ghosts_move
